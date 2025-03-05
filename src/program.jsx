d42d8e519565d8467bf47ec91134594ccd891c25
JithuMorrison <jithus2004@gmail.com>
2025-03-05 21:12:45 +0530

    modified program page

--- src/program.jsx
+++ src/program.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Program = () => {
   return (
-    <div>Program</div>
+    <>
+      <div><span>Download: </span>Scientific Program and List of Presentation</div>
+      <div><span>Download: </span>Book of Abstracts</div>
+    </>
   )
 }
 
